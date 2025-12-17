a940335a7e4ea55836eca72e4a970d52faae7919
dhivyaobuli <34794828+dhivyaobuli@users.noreply.github.com>
2025-12-17 06:20:36 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,29 @@
+
+// Class room Workouts
+//var Person_name = "DhivyaPrabha"
+//var age = 30
+//var Organisation = "Testleaf"
+//var isAutomationTester = true
+//var hasPlaywrightExperience 
+
+
+//console.log("My Name is:"+Person_name + "  "+ "Variable type is:"+typeof(Person_name))
+//console.log("My Age is:"+age + "  "+ "Variable type is : "+typeof(age))
+//console.log("Organisation is:"+Organisation + "  "+"Variable type is:"+typeof(Organisation))
+//console.log("Automation Tester:"+isAutomationTester+"  "+"Variable data type is:"+typeof(isAutomationTester))
+//console.log("Has Playwright Experience:"+hasPlaywrightExperience+ "  "+"Variable data type is :"+typeof(hasPlaywrightExperience))
+
+// Homework for Data Types
+let firstName = "Dhivya"
+let companyName = "TestLeaf"
+let mobileNumber = 123456789
+let isAutomation = true
+let hasPlaywright
+
+
+console.log("First Name : "+firstName + "  "+ "DataType is :" + typeof(firstName))
+console.log("companyName : "+ companyName+"  "+"DataType is :"+typeof(companyName))
+console.log("Mobile Number : "+mobileNumber+"  "+"DataType is :"+typeof(mobileNumber))
+console.log("isAutomation :"+"  "+"DataType is :"+"DataType is :"+typeof(isAutomation))
+console.log("Hasplaywright : "+hasPlaywright+"  "+"Datatype is :"+typeof(hasPlaywright))
+
